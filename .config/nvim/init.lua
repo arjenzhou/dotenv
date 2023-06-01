7ea913fc9d0458c5d3fe4ef3449fd9434697d7de
arjenzhou <github@arjenzhou.com>
2023-06-02 00:16:04 +0800

    neo-tree show dotfiles

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- neotree
+require("neo-tree").setup({
+  filesystem = {
+    filtered_items = {
+      visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+      hide_dotfiles = false,
+      hide_gitignored = true,
+      enable_git_status = true,
+    },
+  },
+})
